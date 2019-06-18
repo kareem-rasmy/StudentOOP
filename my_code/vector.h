2f9d764aec8b92fe614697950120573d557d6c57
Kareem Rasmy <Kareem.Rasmy@gmail.com>
2019-06-18 11:46:10 -0400

    Create vector.h

---
+++ my_code/vector.h
@@ -0,0 +1,34 @@
+
+
+#ifndef MY_CODE_VECTOR_H_
+#define MY_CODE_VECTOR_H_
+const int DEF_CAPACITY = 10;
+
+class MyVec {
+    public:
+        MyVec();
+
+        // copy control:
+        MyVec(const MyVec& v2);
+        ~MyVec();
+        MyVec& operator=(const MyVec& v2);
+
+        void push_back(int val);
+        int size() const { return sz; }
+        int operator[](int i) const;
+        int& operator[](int i);
+    private:
+        int* data;
+        int sz;
+        int capacity;
+};
+
+
+void print_vector(const MyVec& v);
+
+bool operator==(MyVec& v1, MyVec& v2);
+
+
+
+
+#endif /* MY_CODE_VECTOR_H_ */
