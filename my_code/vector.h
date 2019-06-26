dbce199e222330137e7e3b1ed59775da661b6bae
Kareem Rasmy <Kareem.Rasmy@gmail.com>
2019-06-26 15:38:32 -0400

    Update vector.h

--- my_code/vector.h
+++ my_code/vector.h
@@ -1,93 +1,66 @@
-#ifndef VECTOR_H_
-#define VECTOR_H_
+
+#ifndef MY_CODE_VECTOR_H_
+#define MY_CODE_VECTOR_H_
 const int DEF_CAPACITY = 10;
 const int CAPACITY_MULT = 2;
-using namespace std;
-template <typename T>
+
 class MyVec {
-    public:
+public:
+        class Iterator {
+        /*
+         * This class supports ranged for loops.
+         * It includes:
+         * Iterator(int*)
+         * operator++
+         * operator*
+         * operator!=
+         * */
+            friend bool operator!=(Iterator& rhs, Iterator& lhs) {
+                if( rhs.iptr != lhs.iptr) return true;
+                else return false;
+            }
 
+        public:
+            Iterator(int* ip) : iptr(ip) {}
 
+            Iterator& operator++() {
+            	iptr++;
+                return *this;
+            }
 
-	MyVec() : sz(0) {
-		capacity = DEF_CAPACITY;
-	    data = new T[DEF_CAPACITY];
-	}
+            int operator*() { return *iptr; }
 
+        private:
+            int* iptr;
+    };
 
-	MyVec(int sz, T val) : sz{sz} {
-		capacity = max(DEF_CAPACITY, sz * CAPACITY_MULT);
-		data = new T[capacity];
-		for (int i = 0; i < sz; i++) data[i] = val;
-	}
+        MyVec();
+        MyVec(int sz, int val=0);
 
         // copy control:
-	MyVec(const MyVec<T>& v2) {
-		copy(v2);
-	}
-	~MyVec() {
-	    delete [] data;
-	}
+        MyVec(const MyVec& v2);
+        ~MyVec();
+        MyVec& operator=(const MyVec& v2);
 
-	MyVec& operator=(const MyVec<T>& v2) {
-	    if (this != &v2) {
-	        delete [] data;
-	      copy(v2);
-	    }
-	    return *this;
-	}
-
-	void push_back(T val) {
-		sz++;
-	    if (sz > capacity) {
-	        T* old_data = data;
-	        data = new T[capacity * CAPACITY_MULT];
-	        for (int i = 0; i < sz; i++) {
-	        	data[i] = old_data[i];
-	        }
-	        capacity *= CAPACITY_MULT;
-	        delete [] old_data;
-	        }
-	        data[sz - 1] = val;
-	}
+        void push_back(int val);
         int size() const { return sz; }
-
-        T operator[](int i) const {
-            return data[i];
-        }
-
-        T& operator[](int i) {
-            return data[i];
-        }
+        int operator[](int i) const;
+        int& operator[](int i);
+        Iterator begin() const; //return an instance of an iterator class
+        Iterator end() const; //return an instance of an interator class
 
     private:
-        void copy(const MyVec<T>& v2) {
-        	sz = v2.sz;
-            capacity = v2.capacity;
-            data = new T[capacity];
-            for (int i = 0; i < sz; i++) {
-            	data[i] = v2.data[i];
-            }
-        }
-        T* data;
+        void copy(const MyVec& v2);
+        int* data;
         int sz;
         int capacity;
 };
 
 
+void print_vector(const MyVec& v);
 
-template <typename T>
-void print_vector(const MyVec<T>& v) {
-    for (int i = 0; i < v.size(); i++) cout << i << " ";
-    cout << endl;
-}
-template <typename T>
-bool operator==(MyVec<T>& v1, MyVec<T>& v2) {
-	if (v1.size() != v2.size()) return false;
-	for(int i =0; i < v1.size(); i++){
-		if (v1[i] != v2[i]) return false;
-	}
-    return true;
-}
+bool operator==(MyVec& v1, MyVec& v2);
+
+
 
 #endif
